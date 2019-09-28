97028f152ffde7d050d339867d451e49a1ab2716
Matthew Buckley <matthewbuckley@Matthews-MacBook-Air.local>
2019-09-28 15:08:35 -0400

    Generating initial docs

--- metaheuristics/tests/test_optimization_solution.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from metaheuristics.optimization_solution import OptimizationSolution
-
-
-def test_can_create_optimization_solution():
-    solution = OptimizationSolution((2, 3), 15.5)
-    assert solution.get_solution_value() == (2, 3)
-    assert solution.get_objective_value() == 15.5
